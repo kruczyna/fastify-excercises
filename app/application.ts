768a879925c3f26b74d1796ea5cfaeb600a39514
Victoria Kruczek <victoria.kruczek@gmail.com>
2021-08-27 16:04:21 +0200

    Initialize basic route

---
+++ app/application.ts
@@ -0,0 +1,17 @@
+import fastify, { FastifyInstance } from "fastify";
+import { Server, IncomingMessage, ServerResponse } from 'http'
+
+const server: FastifyInstance<
+  Server,
+  IncomingMessage,
+  ServerResponse
+> = fastify()
+
+function build() {
+  server.get('/ping', async (request, reply) => {
+    return 'pong\n'
+  });
+  return server;
+}
+
+export default build;
---
+++ app/index.ts
@@ -0,0 +1,14 @@
+import application from './application'
+
+const PORT: any = process.env.PORT || 3232;
+const fastify = application();
+
+const start = async () => {
+  try {
+    await fastify.listen(PORT, '0.0.0.0');
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+}
+start();
